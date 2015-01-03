82ac4fcd0c201bd8b6dba28620fa1a55bd56e953
Vladimir Skok <skoksworks@gmail.com>
2015-01-03 21:31:39 +0300

    add line number to css output

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,8 @@ gulp.task('default', function() {
 	return gulp.src('application.scss')
     	.pipe(sass({
     		style: 'expanded',
-    		precision: 100
+    		precision: 100,
+        	lineNumbers : true
     	}))
     	.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     	.pipe(gulp.dest('build/'))
